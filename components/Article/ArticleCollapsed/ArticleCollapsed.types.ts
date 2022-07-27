319d26c03a79f2d07e772d2d815a1971a9736f8e
Ruben Kuiper <>
2022-07-27 21:08:00 +0200

    updated typing on card component

--- components/Article/ArticleCollapsed/ArticleCollapsed.types.ts
+++ components/Article/ArticleCollapsed/ArticleCollapsed.types.ts
@@ -3,7 +3,7 @@ import React from "react";
 import { MediaProps } from "../../Media/Media.types";
 
 export default interface ArticleCollapsedProps {
-  id?: string;
+  id?: number | string;
   tags?: Array<String>;
   title?: string;
   media?: MediaProps;
