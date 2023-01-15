de1948126e2c5a78e886572bc46ccb8a0b26e448
Ruben Kuiper <>
2023-01-15 11:20:35 +0100

    created auto generating XML sitemap

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,56 @@
+import { createClient } from "@prismicio/client";
+
+const BASE_URL = "https://www.retroscholar.com";
+
+function generateSiteMap(articles, topics) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+       <loc>${BASE_URL}</loc>
+     </url>
+     ${articles
+       .map(({ article }) => {
+         return `
+       <url>
+           <loc>${`${BASE_URL}/article/${article.uid}`}</loc>
+       </url>
+     `;
+       })
+       .join("")}
+       ${topics
+         .map(({ topic }) => {
+           return `
+        <url>
+            <loc>${`${BASE_URL}/topic/${topic.uid}`}</loc>
+        </url>
+      `;
+         })
+         .join("")}
+   </urlset>
+ `;
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }) {
+  // We make an API call to gather the URLs for our site
+  const client = createClient();
+
+  const articles = await client.getAllByType("story-page");
+  const topics = await client.getAllByType("category-page");
+
+  const sitemap = generateSiteMap(articles, topics);
+
+  res.setHeader("Content-Type", "text/xml");
+
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
