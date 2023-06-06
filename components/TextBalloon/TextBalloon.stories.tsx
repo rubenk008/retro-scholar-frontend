2f5f80aedeea4aaf645ae4be05eafe3d73d4471f
Ruben Kuiper <>
2023-06-06 17:34:23 +0200

    fix: fixed styling of elevated border on mobile

--- components/TextBalloon/TextBalloon.stories.tsx
+++ components/TextBalloon/TextBalloon.stories.tsx
@@ -8,12 +8,21 @@ export default {
   component: TextBalloon,
 };
 export const Basic = () => (
-  <TextBalloon>
-    <Typography variant="body2" component="p" color="white">
-      Cube houses (Dutch: Kubuswoningen) are a set of innovative houses built in
-      Rotterdam in the Netherlands, designed by architect Piet Blom and based on
-      Cube houses (Dutch: Kubuswoningen) are a set of innovative houses built in
-      Rotterdam in the Netherlands, designed by architect Piet Blom and based on
-    </Typography>
-  </TextBalloon>
+  <div
+    style={{
+      height: "100vh",
+      width: "100vw",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <TextBalloon>
+      <Typography variant="body2" component="p" color="white">
+        Cube houses (Dutch: Kubuswoningen) are a set of innovative houses built
+        in Rotterdam in the Netherlands, designed by architect Piet Blom and
+        based on
+      </Typography>
+    </TextBalloon>
+  </div>
 );
--- components/TextBalloon/TextBalloon.tsx
+++ components/TextBalloon/TextBalloon.tsx
@@ -4,13 +4,12 @@ import { TextBalloonProps } from "./TextBalloon.types";
 
 const Wrapper = styled.div`
   position: absolute;
-  z-index: 2;
   width: 34.4rem;
+  margin-left: -1rem;
   padding: 1.8rem 2.4rem;
-  margin: 1rem;
 
   @media screen and (min-width: 768px) {
-    margin: 20rem;
+    margin: auto;
     padding: 3rem 3rem;
     width: 46.9rem;
   }
@@ -34,11 +33,11 @@ const Wrapper = styled.div`
     width: 0;
     height: 0;
     position: absolute;
-    left: 10px;
-    top: -10px;
-    border-left: 10px solid transparent;
-    border-right: 5px solid transparent;
-    border-bottom: 10px solid var(--blue-pigment);
+    left: 1rem;
+    top: -2rem;
+    border-left: 3rem solid transparent;
+    border-right: 1.5rem solid transparent;
+    border-bottom: 3rem solid var(--blue-pigment);
 
     @media screen and (min-width: 768px) {
       left: 2rem;
@@ -61,8 +60,13 @@ const Backdrop = styled.div`
   grid-row: 1;
   grid-column: 1;
   position: absolute;
-  left: 1.05rem;
-  top: -1rem;
+  left: 1rem;
+  top: -0.85rem;
+
+  @media screen and (min-width: 768px) {
+    left: 1.05rem;
+    top: -1rem;
+  }
 
   &:after {
     content: " ";
@@ -70,11 +74,11 @@ const Backdrop = styled.div`
     width: 0;
     height: 0;
     position: absolute;
-    left: 10px;
-    top: -10px;
-    border-left: 10px solid transparent;
-    border-right: 5px solid transparent;
-    border-bottom: 10px solid var(--bay-of-many);
+    left: 0.5rem;
+    top: -1.8rem;
+    border-left: 3rem solid transparent;
+    border-right: 1.5rem solid transparent;
+    border-bottom: 3rem solid var(--bay-of-many);
 
     @media screen and (min-width: 768px) {
       left: 1.5rem;
@@ -99,14 +103,19 @@ const BackdropRightEdge = styled.div`
 
 const BackdropBottomEdge = styled.div`
   position: absolute;
-  bottom:  -1.05rem;
-  right: -0.0rem;
+  bottom: -0.9rem;
+  right: -0.05rem;
   width: 0px;
   height: 0px;
   border-top: 0rem solid transparent;
   border-bottom: 1.1rem solid transparent;
   border-left: 1.1rem solid ${(props) => props.color};
   transform-origin: right center;
+
+  @media screen and (min-width: 768px) {
+    bottom: -1.05rem;
+    right: 0rem;
+  }
 `;
 
 const BackdropTopEdge = styled.div`
@@ -116,20 +125,30 @@ const BackdropTopEdge = styled.div`
   right: 0px;
   background: ${(props) => props.color};
   transform-origin: 0px 100%;
-  height: 2rem;
-  clip-path: polygon(67% 30%, 100% 0, 100% 90%, 60% 100%, 0 100%, 0% 0%);
+  height: 1.2rem;
+  clip-path: polygon(67% 10%, 100% 0, 100% 90%, 60% 100%, 0 100%, 0% 0%);
+
+  @media screen and (min-width: 768px) {
+    height: 2rem;
+    clip-path: polygon(67% 30%, 100% 0, 100% 90%, 60% 100%, 0 100%, 0% 0%);
+  }
 `;
 
 const BackdropLeftEdge = styled.div`
   position: absolute;
-  top: 0;
-  left: -1.05rem;
+  top: -0.01rem;
+  left: -1.07rem;
   width: 0px;
   height: 0px;
   border-left: 1.1rem solid transparent;
   border-right: 0rem solid transparent;
   border-bottom: 1.1rem solid ${(props) => props.color};
   transform-origin: center top;
+
+  @media screen and (min-width: 768px) {
+    top: 0;
+    left: -1.05rem;
+  }
 `;
 
 const TextBalloon = ({ children }: TextBalloonProps) => {
