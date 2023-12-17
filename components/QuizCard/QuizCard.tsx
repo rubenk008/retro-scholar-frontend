73e272cc60dea4b90c4b14b97e2b8164e966572c
Ruben Kuiper <>
2023-12-17 11:49:23 +0100

    Refactor QuizCard component styles for mobile responsiveness

--- components/QuizCard/QuizCard.tsx
+++ components/QuizCard/QuizCard.tsx
@@ -4,19 +4,22 @@ import { motion } from "framer-motion";
 import styled from "styled-components";
 import Props from "./QuizCard.types";
 import { useState } from "react";
+import isMobile from "../../utils/isMobile";
 
 const CardWrapper = styled(motion.div)`
   perspective: 1800px;
   transformstyle: preserve-3d;
   position: relative;
-  height: calc((336 / 414) * 100vw);
-  width: calc((520 / 414) * 100vw);
-  max-height: 495px;
-  max-width: 900px;
+  height: calc((520 / 414) * 100vw);
+  width: calc((336 / 414) * 100vw);
+  max-height: 520px;
+  max-width: 336px;
 
   @media (min-width: 769px) {
     height: calc((495 / 1440) * 100vw);
     width: calc((900 / 1440) * 100vw);
+    max-height: 495px;
+    max-width: 900px;
   }
 `;
 
@@ -53,6 +56,8 @@ const spring = {
 const QuizCard = ({ className, children, ...props }: Props) => {
   const [isFlipped, setIsFlipped] = useState(false);
 
+  const mobile = isMobile();
+
   const handleClick = () => {
     setIsFlipped((prevState) => !prevState);
   };
@@ -61,13 +66,23 @@ const QuizCard = ({ className, children, ...props }: Props) => {
     <CardWrapper onClick={handleClick}>
       <CardFront
         className={clsx(className)}
-        animate={{
-          rotateX: isFlipped ? -180 : 0,
-          y: isFlipped ? "-100%" : 0,
-          boxShadow: isFlipped
-            ? "0px 8px 27px 4px rgba(9,25,189,0)"
-            : "0px 8px 27px 4px rgba(9,25,189,0.2)",
-        }}
+        animate={
+          mobile
+            ? {
+                rotateY: isFlipped ? -180 : 0,
+                x: isFlipped ? "-50%" : 0,
+                boxShadow: isFlipped
+                  ? "0px 8px 27px 4px rgba(9,25,189,0)"
+                  : "0px 8px 27px 4px rgba(9,25,189,0.2)",
+              }
+            : {
+                rotateX: isFlipped ? -180 : 0,
+                y: isFlipped ? "-100%" : 0,
+                boxShadow: isFlipped
+                  ? "0px 8px 27px 4px rgba(9,25,189,0)"
+                  : "0px 8px 27px 4px rgba(9,25,189,0.2)",
+              }
+        }
         transition={spring}
         style={{ zIndex: isFlipped ? 0 : 1 }}
         {...props}
@@ -75,14 +90,26 @@ const QuizCard = ({ className, children, ...props }: Props) => {
         {children}
       </CardFront>
       <CardBack
-        initial={{ rotateX: 180, y: "-100%" }}
-        animate={{
-          rotateX: isFlipped ? 0 : 180,
-          y: isFlipped ? 0 : "-100%",
-          boxShadow: isFlipped
-            ? "0px 8px 27px 4px rgba(9,25,189,0.2)"
-            : "0px 8px 27px 4px rgba(9,25,189,0)",
-        }}
+        initial={
+          mobile ? { rotateY: 180, x: "-50%" } : { rotateX: 180, y: "-100%" }
+        }
+        animate={
+          mobile
+            ? {
+                rotateY: isFlipped ? 0 : 180,
+                x: isFlipped ? 0 : "-50%",
+                boxShadow: isFlipped
+                  ? "0px 8px 27px 4px rgba(9,25,189,0.2)"
+                  : "0px 8px 27px 4px rgba(9,25,189,0)",
+              }
+            : {
+                rotateX: isFlipped ? 0 : 180,
+                y: isFlipped ? 0 : "-100%",
+                boxShadow: isFlipped
+                  ? "0px 8px 27px 4px rgba(9,25,189,0.2)"
+                  : "0px 8px 27px 4px rgba(9,25,189,0)",
+              }
+        }
         transition={spring}
         style={{
           zIndex: isFlipped ? 1 : 0,
